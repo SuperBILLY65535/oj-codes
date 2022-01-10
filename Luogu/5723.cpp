f9a164b03d1b6530750b84dc6dabd0a7d0c69917
Billy <yangmeng10032@bupt.edu.cn>
2022-01-10 13:04:03 +0800

    small modification

--- Luogu/5723.cpp
+++ Luogu/5723.cpp
@@ -1,10 +1,21 @@
 #include <cstdio>
-using namespace std;
-bool Prime(int);
+#include <cmath>
+typedef unsigned long long ull;
+const int max = 100001;
+bool prime(ull k){
+    static bool p[max] = {false, false, true};
+    static ull tmp = 2;
+    if (k<=tmp) return p[k];
+    else {
+        for(int i = 3; i <= std::sqrt(k); i+=2) {
+            if (k % i == 0) {
+                
+            }
+        }
+    }
+}
+int main() {
+    ull n;;
+    std::scanf("%lld", &n);
 
-int main() 
-{
-    int prime[10001], *cur = prime[0], volume, sum = 0;
-    scanf("%d", &volume);
-    
 }
