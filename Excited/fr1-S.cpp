5adc52e15d9d4dbb41df3c2e876def826df53668
SuperBILLY65535 <gdym2002@outlook.com>
2022-01-20 12:55:43 +0800

    solved fr1-S

--- Excited/fr1-S.cpp
+++ Excited/fr1-S.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 
 int main() {
-    int ways[60]; ways[0] = 0; ways[1] = 1;
+    long long ways[60]; ways[0] = 0; ways[1] = 1;
     for(int i = 2; i < 60; i++) {
         ways[i] = ways[i - 1] + ways[i - 2];
     }
@@ -9,7 +9,7 @@ int main() {
     std::scanf("%d", &n);
     while(n--) {
         std::scanf("%d %d", &a, &b);
-        std::printf("%d\n", ways[b - a + 1]);
+        std::printf("%lld\n", ways[b - a + 1]);
     }
     return 0;
 }
