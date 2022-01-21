d28ec3ccfc4e38d545ca7fac21a7decd99c520b2
SuperBILLY65535 <gdym2002@outlook.com>
2022-01-21 17:40:48 +0800

    solved(not yet) fr2-C

--- Excited/fr2-C.cpp
+++ Excited/fr2-C.cpp
@@ -1,32 +1,35 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 #include <algorithm>
 
 inline int dif(const int& lhs, const int& rhs)
 {
-    return std::abs(lhs * lhs - rhs * rhs);
+    return (lhs-rhs)*(lhs-rhs);
 }
 
 int main() {
-    int n, k, weight[2001], dp[2001][2001];
-    while(std::cin >> n >> k)
+    int n, k, weight[2001], dp[2][2001];
+    while(std::scanf("%d %d", &n, &k))
     {
-        for(int i = 1; i <= n; i++) std::cin >> weight[i];
+        std::memset(dp, 0, sizeof(dp));
+        for(int i = 1; i <= n; i++) std::scanf("%d", weight+i);
+
         std::sort(weight, weight+n);
         for(int i = 1; i <= k; i++)
         {
-            for(int j = 2; j <= 2*i; j+=2)
-            {
-                dp[i][2*i] += dif(weight[j-1], weight[j]);
-            }
+            dp[i%2][2*i] = dp[(i+1)%2][2*i-2] + dif(weight[2*i], weight[2*i-1]);
             for(int j = 2*i+1; j <= n; j++)
             {
-                dp[i][j] = std::min(
-                    dp[i][j-1],
-                    (dp[i][j-2] + dif(weight[j], weight[j-1]))
-                );
+                dp[i%2][j] = [] (const int& a, const int& b) ->int
+                {
+                    return a<b?a:b;
+                }
+                (dp[i%2][j-1], dp[(i+1)%2][j-2] + dif(weight[j], weight[j-1]));
             }
         }
-        std::cout << dp[k][n] << '\n';
+
+        std::printf("%d\n", dp[k%2][n]);
     }
     return 0;
 }
