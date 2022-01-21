223a3ae686bcfe30c4427c642533ac06ed549d42
SuperBILLY65535 <gdym2002@outlook.com>
2022-01-21 19:15:13 +0800

    added full-backpack

--- templates/backpack.cpp
+++ templates/backpack.cpp
@@ -17,3 +17,19 @@ T backpack_01(T capacity, T *weight, T *value, size_t items)
     delete[] dp;
     return ans;
 }
+
+template<class T = int>
+T backpack_full(T capacity, T *weight, T *value, size_t items)
+{
+    T *dp = new T[capacity + 1];
+    std::memset(dp, 0, (capacity + 1) * sizeof(T));
+    for(int i = 0; i < items; i++)
+    for(int v = weight[i]; v <= capacity; v++)
+        dp[v] = std::max<T>(
+            dp[v],
+            dp[v-weight[i]] + value[i];
+        );
+    T ans = dp[capacity];
+    delete[] dp;
+    return ans;
+}
