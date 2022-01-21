00fe784f64537c96bd468fa240859d9aca5d6f1c
SuperBILLY65535 <gdym2002@outlook.com>
2022-01-21 19:08:47 +0800

    added backpack template

---
+++ templates/backpack_01.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+template<class T = int>
+T backpack_01(T capacity, T *weight, T *value, size_t items)
+{
+    T *dp = new T[capacity + 1];
+    std::memset(dp, 0, (capacity + 1) * sizeof(T));
+    for(int i = 0; i < items; i++)
+    for(int v = capacity; v >= weight[i]; v--)
+        dp[v] = std::max<T>(
+            dp[v],
+            dp[v-weight[i]] + value[i];
+        );
+    T ans = dp[capacity];
+    delete[] dp;
+    return ans;
+}
