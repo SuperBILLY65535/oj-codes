e9a28080d6c6750e78f935fc66a0029307bfcb05
Billy <xdfzBilly0904@gmail.com>
2022-01-23 23:30:55 +0800

    modified backpack templates

---
+++ templates/backpack_full.h
@@ -0,0 +1,36 @@
+#include <deque>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <cstdio>
+
+typedef int T;
+
+struct item{
+    T value, weight, amount;
+    static item new_item(T _v, T _w, T _a)
+    {
+        item tmp;
+        tmp.value = _v; tmp.weight = _w; tmp.amount = _a;
+        return tmp;
+    }
+};
+
+T backpack_full(
+    const T &capacity, 
+    const std::deque<item> &items
+)
+{
+    T *dp = new T[capacity + 1];
+    std::memset(dp, 0, (capacity + 1) * sizeof(T));
+    for(item i: items)
+    for(T v = i.weight; v <= capacity; v--)
+        dp[v] = std::max<T>(
+            dp[v],
+            dp[v-i.weight] + i.value
+        );
+    T ans = dp[capacity];
+    delete[] dp;
+    return ans;
+}
+
---
+++ templates/backpack_multi.h
@@ -0,0 +1,61 @@
+#include <deque>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <cstdio>
+
+typedef int T;
+
+struct item{
+    T value, weight, amount;
+    static item new_item(T _v, T _w, T _a)
+    {
+        item tmp;
+        tmp.value = _v; tmp.weight = _w; tmp.amount = _a;
+        return tmp;
+    }
+};
+
+T backpack_01(
+    const T &capacity, 
+    const std::deque<item> &items
+)
+{
+    T *dp = new T[capacity + 1];
+    std::memset(dp, 0, (capacity + 1) * sizeof(T));
+    for(item i: items)
+    for(T v = capacity; v >= i.weight; v--)
+        dp[v] = std::max<T>(
+            dp[v],
+            dp[v-i.weight] + i.value
+        );
+    T ans = dp[capacity];
+    delete[] dp;
+    return ans;
+}
+
+T backpack_multi(
+    const T& capacity, 
+    const std::deque<item> &items
+)
+{
+    std::deque<item> it;
+    it.clear();
+    for(auto i: items) 
+    {
+        item tmp = i; int count;
+        for(count = 1; tmp.amount >= count; count *= 2)
+        {
+            it.push_back(tmp);
+            tmp.value *= 2;
+            tmp.weight *= 2;
+            tmp.amount -= count;
+        }
+        if(tmp.amount > 0) it.push_back(item::new_item(
+            i.value * tmp.amount,
+            i.weight * tmp.amount,
+            1
+        ));
+    }
+    return backpack_01(capacity, it);
+}
