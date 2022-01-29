d4ea0e2e38497b1078fd42bc3fa82499617f60a6
SuperBILLY65535 <gdym2002@outlook.com>
2022-01-29 23:04:26 +0800

    solved fr2-Q

---
+++ Excited/fr2-Q.cpp
@@ -0,0 +1,113 @@
+#include <cstdio>
+#include <cstring>
+#include <queue>
+
+char map[200][200];
+bool vis[200][200];
+const int move[4][2] = {
+    {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+};
+
+struct pos{
+    int c, r, step; bool guard;
+    void setv(const int &row, const int &col, const int &st)
+    {
+        c = col; r = row; step = st;
+    }
+    pos() = default;
+    char &stat() const {return map[r][c];}
+    bool checkvis() const {return vis[r][c];}
+    void visit() {vis[r][c] = true;}
+};
+
+bool operator==(const pos &lhs, const pos &rhs)
+{
+    return (lhs.c == rhs.c && lhs.r == rhs.r);
+}
+
+bool within(const int &l, const int &m, const int &n) 
+{
+    return (l <= m && m <= n);
+}
+
+bool boundCheck(const pos &cur, const int &row, const int &col)
+{   
+    // checks if:
+    // the position is legal
+    // the position is not visited and not a wall
+    if(within(0, cur.c, col-1) && within(0, cur.r, row-1))
+    {
+        if(!cur.checkvis() && cur.stat() != '#') return true;
+    }
+    return false;
+}
+
+void bfs(const pos &begin, const int &col, const int &row)
+{
+    std::queue<pos> st; pos cur, tmp;
+    std::memset(vis, false, sizeof(vis));
+    st.push(begin);
+
+    while(!st.empty())
+    {
+        cur = std::move(st.front()); 
+        st.pop();
+
+        if(cur.stat() == 'x' && !cur.guard) // guard not eliminated yet
+        {
+            cur.guard = true;
+            cur.step++;
+            st.push(cur);
+            continue;
+        }
+
+        cur.guard = false;
+        for(int i = 0; i < 4; i++) 
+        {
+            tmp.setv(cur.r + move[i][0], cur.c + move[i][1], cur.step + 1);
+            if(boundCheck(tmp, row, col))
+            {
+                if(tmp.stat() == 'a')
+                {
+                    std::printf("%d\n", tmp.step);
+                    return;
+                }
+                tmp.visit();
+                st.push(std::move(tmp));
+            }
+        }
+    }
+    std::printf("Poor ANGEL has to stay in the prison all his life.\n");
+}
+
+int main()
+{
+    int col, row; 
+    pos begin;
+    // freopen("q-in.txt", "r", stdin);
+    while(std::scanf("%d %d", &row, &col) == 2)
+    {
+        for(int r = 0; r < row; r++)
+        {
+            scanf("%s", map[r]);
+            for(int c = 0; c < col; c++)
+            {
+                if(map[r][c] == 'r')
+                {
+                    begin.setv(r, c, 0);
+                }
+            }
+            std::scanf("\n");
+        }
+        // for(int r = 0; r < row; r++)
+        // {
+        //     for(int c = 0; c < col; c++)
+        //     {
+        //         std::printf("%c", map[r][c]);
+        //     }
+        //     std::printf("\n");
+        // }
+        bfs(begin, col, row);
+    }
+    return 0;
+} 
