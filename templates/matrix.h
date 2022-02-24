c0d560e229ee14e4a4f8e359d58b5d280a4a4210
SuperBILLY65535 <gdym2002@outlook.com>
2022-02-24 09:55:03 +0800

    solved int1-G

--- templates/matrix.h
+++ templates/matrix.h
@@ -3,132 +3,91 @@
 #include <utility>
 #include <cassert>
 
-// #define modulus 9973
+#define modulus 9973
 
 typedef int Numtype;
 
-struct matrix{
+class Matrix{
     std::vector<std::vector<Numtype>> mat;
-    /**
-     * @brief Construct a new matrix object by size and default insert value
-     * 
-     * @param row Size of row
-     * @param col Size of column
-     * @param value Default insert value
-     */
-    matrix(const size_t & row, const size_t & col, const Numtype &&value = 0) {
-        mat.resize(row);
-        for(auto &r: mat) r.resize(col, value);
+public:
+    std::vector<Numtype> &operator[] (const size_t &_index) {
+        return this->mat[_index];
     }
-    /**
-     * @brief returns the number of columns
-     * 
-     * @return size_t&& number of columns
-     */
-    size_t &&col() const {
-        return mat[0].size();
+    Matrix(const size_t &row, const size_t &col, const Numtype &elem = 0) {
+        this->mat.resize(row);
+        for(auto &_row: mat) 
+            _row.resize(col, elem);
     }
-    /**
-     * @brief Returns the number of rows
-     * 
-     * @return size_t&& number of rows
-     */
-    size_t &&row() const {
+    size_t row() const {
         return mat.size();
     }
-    /**
-     * @brief Checks if a matrix is square
-     * 
-     * @return true col() == row()
-     * @return false otherwise
-     */
-    bool isSquare() const {
-        return col() == row();
+    size_t col() const {
+        return mat[0].size();
     }
-    std::vector<Numtype> &operator[] (const size_t &&rindex) {
-        return this->mat[rindex];
-    }
-    /**
-     * @brief Returns the trace of a square matrix
-     * 
-     * @return Numtype&& Tr(*this)
-     */
-    Numtype &&trace() const {
-        assert(col() == row());
+    Numtype trace() const {
+        assert(this->row() == this->col());
         Numtype ans = 0;
-        for(size_t i = 0, s = row(); i < s; i++)
-            ans += mat[i][i];
-        return std::move(ans);
+        for(size_t k = 0, r = this->row(); k < r ; k++) {
+            #ifdef modulus
+            ans += this->mat[k][k] % modulus;
+            ans %= modulus;
+            #else
+            ans += this->mat[k][k];
+            #endif
+        }
+        return std::move(ans); 
     }
-    /**
-     * @brief Constructs an identity matrix of size s
-     * 
-     * @param s size 
-     * @return matrix&& An identity matrix sized s
-     */
-    static matrix &&identity(const size_t && s) {
-        matrix I(s, s);
-        for(int i = 0; i < s; i++) I[i][i] = 1;
-        return std::move(I);
+    static Matrix identity(const size_t &s) {
+        assert(s > 0);
+        Matrix ans(s, s);
+        for(size_t i = 0; i < s; i++)
+            ans[i][i] = 1;
+        return ans;
     }
 };
 
-matrix &&operator+ (matrix &lhs, matrix &rhs) {
-    assert(lhs.row() == rhs.row() && lhs.col() == rhs.col());
-    matrix ans = lhs;
-    for(int r = 0; r < lhs.row(); r++) {
-        for(int c = 0; c < lhs.row(); c++) {
-            ans[r][c] += rhs[r][c];
-        }
-    }
-    return std::move(ans);
-}
-
-matrix &&operator* (matrix &lhs, const int &rhs) {
-    matrix ans = lhs;
-    for(auto &r: ans.mat)
-    for(auto &i: r)
-        #ifdef modulus
-        i = i * (rhs % modulus) % modulus;
-        #else
-        i = i * rhs;
-        #endif
-    return std::move(ans);
-}
-
-matrix &&operator* (matrix &lhs, matrix &rhs) {
-    assert(lhs.col() == rhs.row());
-    matrix ans(lhs.row(), rhs.col());
-    Numtype tmp;
-    for(int r = 0; r < ans.row(); r++)
-    for(int c = 0; c < ans.col(); c++)
-    {
-        tmp = 0;
-        for(int i = 0; i < lhs.col(); i++) {
+Matrix operator+ (Matrix &lhs, Matrix &rhs) {
+    assert(lhs.col() == rhs.col() && lhs.row() == rhs.row());
+    Matrix ans(lhs.row(), lhs.col());
+    for(size_t r = 0; r < ans.row(); r++) {
+        for(size_t c = 0; c < ans.col(); c++) {
             #ifdef modulus
-            tmp += lhs.mat[r][i] * rhs.mat[i][c] % modulus;
-            tmp %= modulus;
+            ans[r][c] = lhs[r][c] + rhs[r][c] % modulus;
             #else
-            tmp += lhs[r][i] * rhs[i][c];
+            ans[r][c] = lhs[r][c] + rhs[r][c];
             #endif
         }
-        ans[r][c] = std::move(tmp);
     }
-    return std::move(ans);
+    return ans;
 }
 
-/**
- * @brief Returns Mat ^ t with logarithmic time consumption
- *  
- */
-matrix &&pow(matrix &const Mat, int &const t) {
-    assert(t <= 0 || Mat.isSquare());
-    matrix tmp = matrix::identity(Mat.row()), ans = tmp;
-    while(t > 0)
-    {
-        tmp = tmp * Mat;
-        if(t % 2) ans = ans * tmp;
-        t /= 2;
+Matrix operator* (Matrix &lhs, Matrix &rhs) {
+    assert(lhs.col() == rhs.row());
+    Matrix ans(lhs.row(), rhs.col());
+    for(size_t r = 0; r < ans.row(); r++) {
+        for(size_t c = 0; c < ans.col(); c++) {
+            for(size_t k = 0; k < lhs.col(); k++) {
+                #ifdef modulus
+                ans[r][c] += lhs[r][k] * rhs[k][c] % modulus;
+                ans[r][c] %= modulus;
+                #else
+                ans[r][c] += lhs[r][k] * rhs[k][c];
+                #endif
+            }
+        }
     }
-    return std::move(ans);
+    return ans;
+}
+
+Matrix pow(Matrix &mat, int p) {
+    assert(p >= 0);
+    assert(mat.row() == mat.col());
+
+    Matrix ans(Matrix::identity(mat.row())), tmp(mat);
+    while(p > 0) {
+        if(p % 2) ans = ans * tmp;
+        p >>= 1;
+        tmp = tmp * tmp;
+    }
+    return ans;
 }
